d92878b14d5ed77614d13a6242d1b012053a5c42
shahpurnima <shahpurnima2@gmail.com>
2014-07-15 22:59:58 +0530

    category page php

---
+++ category_page.php
@@ -0,0 +1,142 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Book Store</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link href="lib/css/bootstrap.css" rel="stylesheet">
+<link href="my_css/mycss_main.css" rel="stylesheet" type="text/css">
+<link href="my_css/titlebar.css" rel="stylesheet" type="text/css">
+<link href="my_css/login.css" rel="stylesheet" type="text/css">
+<link href="my_css/categorybar.css" rel="stylesheet" type="text/css">
+<link href="my_css/categorylist.css" rel="stylesheet" type="text/css">
+    <!--[if lt IE 9]>
+    <script src=lib/js/html5shiv.js></script>
+    <script src="lib/js/respond.min.js"></script>
+    <![endif]-->
+<script type="text/javascript">
+var js_checkcode = "checkcode";
+</script>
+</head>
+
+<body>
+<?php include("include/titlebar.php"); ?>
+<?php include("include/login.php"); ?>
+<?php include("include/categorybar.php"); ?>
+<div id="main_page">
+  <div id="main_page_right">
+    <div id="content_books">
+      <div style="height:40px; width:200px"></div>
+      <div class="search_result">
+        <div class="book_item_for_search"> <a href="#"><img src="images/Finding Cinderella.jpg"/></a>
+          <div class="book_item_intro"> <a class="book_name_link" href="#">Finding Cinderella <span class="book_name_end"></span></a><br/>
+            <a class="author_name_link" href="#">Colleen Hoover</a> </div>
+        </div>
+        <div class="book_item_for_search"> <a href="#"><img src="images/Finding Cinderella.jpg"/></a>
+          <div class="book_item_intro"> <a class="book_name_link" href="#">Finding Cinderella <span class="book_name_end"></span></a><br/>
+            <a class="author_name_link" href="#">Colleen Hoover</a> </div>
+        </div>
+        <div class="book_item_for_search"> <a href="#"><img src="images/Finding Cinderella.jpg"/></a>
+          <div class="book_item_intro"> <a class="book_name_link" href="#">Finding Cinderella <span class="book_name_end"></span></a><br/>
+            <a class="author_name_link" href="#">Colleen Hoover</a> </div>
+        </div>
+        <div class="book_item_for_search"> <a href="#"><img src="images/Finding Cinderella.jpg"/></a>
+          <div class="book_item_intro"> <a class="book_name_link" href="#">Finding Cinderella <span class="book_name_end"></span></a><br/>
+            <a class="author_name_link" href="#">Colleen Hoover</a> </div>
+        </div>
+        <div class="book_item_for_search"> <a href="#"><img src="images/Finding Cinderella.jpg"/></a>
+          <div class="book_item_intro"> <a class="book_name_link" href="#">Finding Cinderella <span class="book_name_end"></span></a><br/>
+            <a class="author_name_link" href="#">Colleen Hoover</a> </div>
+        </div>
+      </div>
+      <div class="search_result">
+        <div class="book_item_for_search"> <a href="#"><img src="images/Finding Cinderella.jpg"/></a>
+          <div class="book_item_intro"> <a class="book_name_link" href="#">Finding Cinderella <span class="book_name_end"></span></a><br/>
+            <a class="author_name_link" href="#">Colleen Hoover</a> </div>
+        </div>
+        <div class="book_item_for_search"> <a href="#"><img src="images/Finding Cinderella.jpg"/></a>
+          <div class="book_item_intro"> <a class="book_name_link" href="#">Finding Cinderella <span class="book_name_end"></span></a><br/>
+            <a class="author_name_link" href="#">Colleen Hoover</a> </div>
+        </div>
+        <div class="book_item_for_search"> <a href="#"><img src="images/Finding Cinderella.jpg"/></a>
+          <div class="book_item_intro"> <a class="book_name_link" href="#">Finding Cinderella <span class="book_name_end"></span></a><br/>
+            <a class="author_name_link" href="#">Colleen Hoover</a> </div>
+        </div>
+        <div class="book_item_for_search"> <a href="#"><img src="images/Finding Cinderella.jpg"/></a>
+          <div class="book_item_intro"> <a class="book_name_link" href="#">Finding Cinderella <span class="book_name_end"></span></a><br/>
+            <a class="author_name_link" href="#">Colleen Hoover</a> </div>
+        </div>
+        <div class="book_item_for_search"> <a href="#"><img src="images/Finding Cinderella.jpg"/></a>
+          <div class="book_item_intro"> <a class="book_name_link" href="#">Finding Cinderella <span class="book_name_end"></span></a><br/>
+            <a class="author_name_link" href="#">Colleen Hoover</a> </div>
+        </div>
+      </div>
+      <div class="search_result">
+        <div class="book_item_for_search"> <a href="#"><img src="images/Finding Cinderella.jpg"/></a>
+          <div class="book_item_intro"> <a class="book_name_link" href="#">Finding Cinderella <span class="book_name_end"></span></a><br/>
+            <a class="author_name_link" href="#">Colleen Hoover</a> </div>
+        </div>
+        <div class="book_item_for_search"> <a href="#"><img src="images/Finding Cinderella.jpg"/></a>
+          <div class="book_item_intro"> <a class="book_name_link" href="#">Finding Cinderella <span class="book_name_end"></span></a><br/>
+            <a class="author_name_link" href="#">Colleen Hoover</a> </div>
+        </div>
+        <div class="book_item_for_search"> <a href="#"><img src="images/Finding Cinderella.jpg"/></a>
+          <div class="book_item_intro"> <a class="book_name_link" href="#">Finding Cinderella <span class="book_name_end"></span></a><br/>
+            <a class="author_name_link" href="#">Colleen Hoover</a> </div>
+        </div>
+        <div class="book_item_for_search"> <a href="#"><img src="images/Finding Cinderella.jpg"/></a>
+          <div class="book_item_intro"> <a class="book_name_link" href="#">Finding Cinderella <span class="book_name_end"></span></a><br/>
+            <a class="author_name_link" href="#">Colleen Hoover</a> </div>
+        </div>
+        <div class="book_item_for_search"> <a href="#"><img src="images/Finding Cinderella.jpg"/></a>
+          <div class="book_item_intro"> <a class="book_name_link" href="#">Finding Cinderella <span class="book_name_end"></span></a><br/>
+            <a class="author_name_link" href="#">Colleen Hoover</a> </div>
+        </div>
+      </div>
+      <div class="search_result">
+        <div class="book_item_for_search"> <a href="#"><img src="images/Finding Cinderella.jpg"/></a>
+          <div class="book_item_intro"> <a class="book_name_link" href="#">Finding Cinderella <span class="book_name_end"></span></a><br/>
+            <a class="author_name_link" href="#">Colleen Hoover</a> </div>
+        </div>
+        <div class="book_item_for_search"> <a href="#"><img src="images/Finding Cinderella.jpg"/></a>
+          <div class="book_item_intro"> <a class="book_name_link" href="#">Finding Cinderella <span class="book_name_end"></span></a><br/>
+            <a class="author_name_link" href="#">Colleen Hoover</a> </div>
+        </div>
+        <div class="book_item_for_search"> <a href="#"><img src="images/Finding Cinderella.jpg"/></a>
+          <div class="book_item_intro"> <a class="book_name_link" href="#">Finding Cinderella <span class="book_name_end"></span></a><br/>
+            <a class="author_name_link" href="#">Colleen Hoover</a> </div>
+        </div>
+        <div class="book_item_for_search"> <a href="#"><img src="images/Finding Cinderella.jpg"/></a>
+          <div class="book_item_intro"> <a class="book_name_link" href="#">Finding Cinderella <span class="book_name_end"></span></a><br/>
+            <a class="author_name_link" href="#">Colleen Hoover</a> </div>
+        </div>
+        <div class="book_item_for_search"> <a href="#"><img src="images/Finding Cinderella.jpg"/></a>
+          <div class="book_item_intro"> <a class="book_name_link" href="#">Finding Cinderella <span class="book_name_end"></span></a><br/>
+            <a class="author_name_link" href="#">Colleen Hoover</a> </div>
+        </div>
+      </div>
+    </div>
+    <div id="footer">
+      <div style="height:50px">
+        <hr style="width:600px; margin-left:50px">
+      </div>
+      <div id="footer_link"> <span style="font-size:12px; color:#555;">©2013 Scatter Inc.</span> <a class="footer_linker_item" href="#">Site Terms of Service</a> <a class="footer_linker_item" href="#">Privacy Policy</a> <a class="footer_linker_item" href="#">Developers</a> <a class="footer_linker_item" href="#">About Us</a> </div>
+    </div>
+  </div>
+</div>
+<script type="text/javascript" src="lib/js/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="lib/js/stickUp.min.js"></script>
+<script type="text/javascript" type="text/javascript">
+    jQuery(function($) {
+        $(document).ready( function() {
+            $('#category_title').stickUp();
+        });
+    });
+</script>
+<script type="text/javascript" src="lib/js/bootstrap.js"></script>
+<script type="text/javascript" src="my_js/my_js.js"></script>
+</body>
+</html>
